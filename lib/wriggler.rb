f3f6b0a3fe71f9890f1c3826f640a8b0c372e199
Elliott Young <elliott.a.young@gmail.com>
2015-06-06 10:40:05 -0500

    Build skeleton of module

--- lib/wriggler.rb
+++ lib/wriggler.rb
@@ -1,5 +1,24 @@
 require "wriggler/version"
 
 module Wriggler
-  # Your code goes here...
+  def crawl_HTML(directory, tags=[])
+  	files = open_directory(directory)
+
+  	files.each do |file|
+  		doc = Nokogiri::HTML(file)
+
+  	end
+  end
+
+  private
+
+  def open_directory(directory)
+  	files = Dir.entries(directory)
+  end
+
+  def open_next_file
+  end
+end
+
+module Writer
 end

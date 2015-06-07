ba31248871f36e1cd46835bd1d0b5c8761bd5c61
Elliott Young <elliott.a.young@gmail.com>
2015-06-07 17:00:53 -0500

    Build Test Skeleton

--- spec/wriggler_spec.rb
+++ spec/wriggler_spec.rb
@@ -4,4 +4,36 @@ describe Wriggler do
   it 'has a version number' do
     expect(Wriggler::VERSION).not_to be nil
   end
+
+  context '#crawl' do
+  end
+
+  context '#navigate_directory' do
+  end
+
+  context '#gather_files' do
+  end
+
+  context '#open_next_file' do
+  end
+
+  context '#is_HTML?' do
+  end
+
+  context '#is_XML?' do
+  end
+
+  context '#set_HTML' do
+  end
+
+  context '#crawl_file' do
+  end
+
+  context '#sanitize' do
+  end
+end
+
+describe Writer do
+	context '#write_to_csv' do
+	end
 end
